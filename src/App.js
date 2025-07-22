2b8b13a999f1b843d22166515c4d47202dede9af
DHURANTHAM007 <anjosh753@gmail.com>
2025-07-22 21:28:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box, Stack } from '@mui/material';
@@ -7,23 +6,17 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import VideoDetail from './components/VideoDetail';
-// Import SearchResults when you create it
-// import SearchResults from './components/SearchResults';
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: '#0F0F0F' }}>
+    <Box sx={{ backgroundColor: '#0F0F0F', color: 'white' }}>
       <Navbar />
-      <Stack direction="row" spacing={{ xs: 0, sm: 2 }}>
-        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-          <Sidebar />
-        </Box>
-        
-        <Box sx={{ width: '100%' }}>
+      <Stack direction="row">
+        <Sidebar />
+        <Box sx={{ width: '100%', height: 'calc(100vh - 64px)', overflowY: 'auto' }}>
           <Routes>
             <Route path="/" exact element={<Feed />} />
             <Route path="/video/:id" element={<VideoDetail />} />
-            {/* <Route path="/search/:searchTerm" element={<SearchResults />} /> */}
           </Routes>
         </Box>
       </Stack>
